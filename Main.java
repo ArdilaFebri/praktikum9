f0ec5a386f2815c4bdc66ead46e1dcd7fbc3a8c1
ArdilaFebri <133100284+ArdilaFebri@users.noreply.github.com>
2023-07-12 16:16:33 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 public class Main{
-
     public static void main(String[] args){
 
         Buku buku1 = new Buku ("B001", "Pemrograman Java");
@@ -8,13 +7,15 @@ public class Main{
 
         Petugas petugas1 = new Petugas ("Alex", "P001");
         Peminjam peminjam1 = new Mahasiswa();
-
         peminjam1.setNamaPeminjam("Budi");
         peminjam1.setKodePeminjam("M0001");
 
-        peminjam1.doLogin();
-        // peminjam1.doLogout();
+        Peminjam peminjam2 = new Mahasiswa();
+        peminjam2.setNamaPeminjam("Ardila");
+        peminjam2.setKodePeminjam("Ayu");
 
+        peminjam1.doLogin();
+        peminjam2.doLogin();
 
         try{
             Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku2);
@@ -23,12 +24,37 @@ public class Main{
             System.out.println("Kode Peminjam: " + peminjaman1.getPeminjam().getKodePeminjam());
             System.out.println("Nama Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
             System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku());
-        } catch(Exception e){
-            System.out.println("Terjadi kesalahan  : " + e.getMessage());
+            Denda denda1 = new Denda(peminjaman1, hariTerlambat:5);
+            System.out.println ("Tagihan denda: " + denda1.getTagihan());
+
+            System.out.println ("===================");
+
+            Peminjaman peminjaman2 = new Peminjaman(peminjam2, petugas1, buku3);
+
+            System.out.println("Nama Peminjam: " + peminjaman2.getPeminjam().getNamaPeminjam());
+            System.out.println("Kode Peminjam: " + peminjaman1.getPeminjam().getKodePeminjam());
+            System.out.println("Nama Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
+            System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku());
+            Denda denda2 = new Denda(peminjaman2, hariTerlambat:7);
+            System.out.println("Tagihan denda: " + denda2.getTagihan());
+
+            System.out.println("== Denda dihapuskan ! ==");
+
+            peminjam2.addAntiDenda();
+
+            System.out.println("Tagihan denda: " + denda2.getTagihan());
+
+            System.out.println("== Denda ditambahakan ! ==");
+
+            peminjam2.removeAntiDenda();
+
+            System.out.println("tagihan denda: " + denda2.getTagihan());
+
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
         } finally{
             System.out.println("Program Selesai");
+    
         }
-
     }
-
 }
