4a36d6fabc5fb127b49b3ca9c98ac36c099a8504
ArdilaFebri <133100284+ArdilaFebri@users.noreply.github.com>
2023-07-12 16:15:33 +0700

    Update Peminjam.java

--- Peminjam.java
+++ Peminjam.java
@@ -17,9 +17,13 @@ public abstract class Peminjam implements UserLogin{
     public boolean isLogin(){
         return isLogin;
     }
+    private boolean isAntiDenda;
 
     public abstract void setKodePeminjam(String kodePeminjam);
     public abstract String getKodePeminjam();
     public abstract void setNamaPeminjam(String namaPeminjam);
     public abstract String getNamaPeminjam();
+    public abstract void addAntiDenda();
+    public abstract void removeAntiDenda();
+    public abstract boolean getAntiDenda();
 }
